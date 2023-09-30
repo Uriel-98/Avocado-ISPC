cc3b0eb5ebd24ee4f705dc7f9c2d872e2775f54e
Emilia <emirod467@gmail.com>
2023-09-30 14:22:34 -0300

    Activity Iniciar Sesión

---
+++ app/src/main/java/com/example/proyectoavocado/IniciarSesionActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectoavocado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IniciarSesionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+    }
+}
