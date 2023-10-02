920db8a7c7b92298c53c7223c8269ad095fe2f65
Yanina <unrein.syanina@gamil.com>
2023-10-01 23:15:09 -0300

    Activity Registrarse arreglada

--- app/src/main/java/com/example/proyectoavocado/favoritesActivity.java
+++ app/src/main/java/com/example/proyectoavocado/favoritesActivity.java
@@ -2,7 +2,10 @@ package com.example.proyectoavocado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class favoritesActivity extends AppCompatActivity {
 
@@ -10,5 +13,58 @@ public class favoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        //capturo los id de los botones
+        ImageButton btnBackHome = findViewById(R.id.btn_volverHome);
+        ImageButton btnHome = findViewById(R.id.btn_home);
+        ImageButton btnBuscarReceta = findViewById(R.id.btn_buscar);
+        ImageButton btnAgregarReceta = findViewById(R.id.btn_agregar);
+        ImageButton btnFavoritos = findViewById(R.id.btn_favoritos);
+        ImageButton btnPerfil = findViewById(R.id.btn_perfil);
+
+        btnBackHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crear un Intent para abrir FeedActivity
+                Intent intent = new Intent(favoritesActivity.this, FeedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crear un Intent para abrir FeedActivity
+                Intent intent = new Intent(favoritesActivity.this, FeedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnAgregarReceta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crear un Intent para abrir AgregarRecetaActivity
+                Intent intent = new Intent(favoritesActivity.this, AgregaRecetaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFavoritos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crear un Intent para abrir FavoritosActivity
+                Intent intent = new Intent(favoritesActivity.this, favoritesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crear un Intent para abrir PerfilActivity
+                Intent intent = new Intent(favoritesActivity.this, PerfilActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
