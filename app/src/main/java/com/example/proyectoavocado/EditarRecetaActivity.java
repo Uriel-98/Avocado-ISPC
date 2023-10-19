d07e5508d16f86a1b933a5fc7f55951c81b5568a
Florencia <riosflorenciaitati@gmail.com>
2023-10-19 18:53:40 -0300

    Cree Activity Editar Receta

---
+++ app/src/main/java/com/example/proyectoavocado/EditarRecetaActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectoavocado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarRecetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_receta);
+    }
+}
