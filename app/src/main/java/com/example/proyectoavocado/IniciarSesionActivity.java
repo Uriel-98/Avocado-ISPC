5489232f181a6286b3158f6861b1b4f88160171a
Lorena Aylen <lag9512@gmail.com>
2023-10-20 19:51:20 -0300

    Modelo de Api Rest + configuraciones

--- app/src/main/java/com/example/proyectoavocado/IniciarSesionActivity.java
+++ app/src/main/java/com/example/proyectoavocado/IniciarSesionActivity.java
@@ -2,10 +2,24 @@ package com.example.proyectoavocado;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class IniciarSesionActivity extends AppCompatActivity {
@@ -14,6 +28,7 @@ public class IniciarSesionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
 
         //capturo los id de los botones
         ImageButton btnVolver = findViewById(R.id.btn_volver);
@@ -33,10 +48,14 @@ public class IniciarSesionActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Crear un Intent para abrir FeedActivity
-                Intent intent = new Intent(IniciarSesionActivity.this, FeedActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(IniciarSesionActivity.this, FeedActivity.class);
+                //startActivity(intent);
+                llamar();
+
+
             }
         });
+
 
         btnRegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,4 +66,27 @@ public class IniciarSesionActivity extends AppCompatActivity {
             }
         });
     }
+
+private void llamar(){
+        String url = "https://fakestoreapi.com/products/1";
+
+    StringRequest get = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+        try {
+            JSONObject json = new JSONObject(response);
+            String title = json.getString("title");
+            Toast.makeText(getApplicationContext(), title, Toast.LENGTH_LONG).show();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        }
+    }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e("Error", error.getMessage());
+        }
+    });
+    Volley.newRequestQueue(this).add(get);
+}
 }
