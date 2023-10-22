28e84cd0f38670c078d37ee75f34573b2bb6d8b5
Lorena Aylen <lag9512@gmail.com>
2023-10-22 00:08:25 -0300

    Se creó el método DELETE para el usuario

--- Backend/routes/usuario.js
+++ Backend/routes/usuario.js
@@ -1,5 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../conection')
 
+router.delete('/eliminar', (req, res) => {
+  const email = req.body.email
+ console.log(email)
+ if(typeof email === 'undefined'){
+  res.status(400).json('No se encontró el email')
+  return
+ }
+db.query(`DELETE FROM usuarios WHERE email = '${email}'`, function(error, results){
+  if(error){
+    res.send(error)
+  }
+  else{
+    req.session.destroy(err => {
+      if(err){
+        res.send(err)
+        return
+      }
+    })
+    res.send({
+      success: true,
+      message: 'Usuario eliminado correctamente'
+    })
+  }
+})
+
+})
 
 module.exports = router
