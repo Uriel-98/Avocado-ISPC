bf924dde040298692bad5c987cfc5d225640742b
FlorenciaItati <riosflorenciaitati@gmail.com>
2023-10-21 21:35:15 -0300

    activity dlor

---
+++ app/src/main/java/com/example/proyectoavocado/ModificarRecetaActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectoavocado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModificarRecetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_receta);
+    }
+}
