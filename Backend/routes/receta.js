35d693c3e2c3fd76c3aaf672037943a1e490ed52
Lorena Aylen <lag9512@gmail.com>
2023-10-22 21:39:30 -0300

    Se agregaron endpoints GET
    Se agregó endpoint para traer una lista de recetas para el feed. Se agregó el endpoint que trae la información de una receta.

--- Backend/routes/receta.js
+++ Backend/routes/receta.js
@@ -1,5 +1,42 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../conection')
 
+router.get('/getRecetasFeed', (req, res) => {
+  db.query(`SELECT  r.idReceta, r.titulo, u.usuario AS creadoPor, r.imagen, r.fechaCreacion, r.fechaActualizacion FROM recetas r INNER JOIN usuarios u ON u.idUsuario = r.creadoPor LIMIT 20;`, function(error, results){
+    if(error){
+      res.send({
+        success:false,
+        message: error
+      })
+    } else {
+      res.send({
+        success:true,
+        message: '',
+        content: results
+      })
+    }
+  })
+})
+
+router.get('/getRecetaById/:id', (req, res)=>{
+  const idReceta = req.params.id
+  db.query(`CALL sp_getReceta(${idReceta});`, function (error, results){
+    if(error){
+      res.send({
+        success:false,
+        message: error
+      })
+    } 
+
+    else {
+      res.send(results[0][0])
+      return
+    }
+    
+  })
+
+  
+})
 
 module.exports = router
