1f614477b522508c60eb35f5d4858d3e435e6c0d
Uriel <urield1917@gmail.com>
2023-10-25 23:07:38 -0300

    modificación al layout de Agregar receta empezando la lógica

--- app/src/main/java/com/example/proyectoavocado/AgregaRecetaActivity.java
+++ app/src/main/java/com/example/proyectoavocado/AgregaRecetaActivity.java
@@ -1,11 +1,16 @@
 package com.example.proyectoavocado;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class AgregaRecetaActivity extends AppCompatActivity {
 
@@ -15,7 +20,17 @@ public class AgregaRecetaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_agrega_receta);
 
         ImageButton btnHome = findViewById(R.id.btn_back);
-
+        // Inicializa los EditText
+        EditText editTextTitulo = findViewById(R.id.text_title);
+        EditText editTextDescripcion = findViewById(R.id.text_descripcion);
+        EditText editTextTiempoCoccion = findViewById(R.id.text_tiempoCoccion);
+        EditText editTextDificultad = findViewById(R.id.text_tiempoCoccion);
+        Button btn_ingredientes = findViewById(R.id.btn_ingredientes);
+        LinearLayout contenedorIngredientes = findViewById(R.id.contenedor_ingredientes);
+        Button btn_pasos = findViewById(R.id.btn_pasos);
+        LinearLayout contenedor_pasos = findViewById(R.id.contenedor_pasos);
+        Button btn_categorias = findViewById(R.id.btn_categorias);
+        LinearLayout contenedor_categorias = findViewById(R.id.contenedor_categorias);
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,5 +39,11 @@ public class AgregaRecetaActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+
+
+
+        
+   }
+
 }
+
