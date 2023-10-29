b76609f805e25ce3191225ea8c87899850ce0785
Yanina-Unrein <unrein.syanina@gmail.com>
2023-10-28 21:33:01 -0300

    intentando poner videos y corrigiendo conflictos

--- app/src/main/java/com/example/proyectoavocado/reciclesAdaptadores/PasosRecetaRecipeAdapter.java
+++ app/src/main/java/com/example/proyectoavocado/reciclesAdaptadores/PasosRecetaRecipeAdapter.java
@@ -1,15 +1,13 @@
 package com.example.proyectoavocado.reciclesAdaptadores;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.proyectoavocado.R;
